63167d8a981beb1f71f90164212476589596f68c
NAZMUS SAKIB <sakibabir52@gmail.com>
2021-10-07 00:27:29 +0600

    Post section complete

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose'); 
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
+const postRoute = require('./routes/posts');
 
 dotenv.config();
 app.use(express.json());
@@ -15,8 +16,9 @@ mongoose
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 
-app.listen(8000, ()=> {
+app.listen(5000, ()=> {
     console.log('backend is running');
 })
